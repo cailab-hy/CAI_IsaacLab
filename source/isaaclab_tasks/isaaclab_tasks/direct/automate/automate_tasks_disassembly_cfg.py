38463051d940c6ecf5e23e6444dfe607305c1382
Jisub Shin <159870069+JSSh1n@users.noreply.github.com>
2025-03-28 21:54:35 +0900

    Update automate_tasks_disassembly_cfg.py
    Signed-off-by: Jisub Shin <159870069+JSSh1n@users.noreply.github.com>

--- source/isaaclab_tasks/isaaclab_tasks/direct/automate/automate_tasks_disassembly_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/automate/automate_tasks_disassembly_cfg.py
@@ -13,14 +13,7 @@ from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
 ASSET_DIR = f"{ISAACLAB_NUCLEUS_DIR}/Factory"
 AUTOMATE_ASSET_DIR = os.getcwd() + f"/source/isaaclab_assets/data/automate_usd"
 
-# TODO
-''' add plz
-data_dir
-disassembly_dist_file
-desired_subassemblies
-'''
-
-
+tmp_mass = 0.019
 
 @configclass
 class FixedAssetCfg:
@@ -95,32 +88,34 @@ class AutomateTaskDisassembly:
     success_threshold: float = 0.04
     engage_threshold: float = 0.9
 
+################################### Template ##############################################################
+
 @configclass
-class Plug_00117(HeldAssetCfg):
-    usd_path = f"{AUTOMATE_ASSET_DIR}/00117_plug/00117_plug.usd"
-    
+class Plug_00000(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00004_plug/00004_plug.usd"
+
     # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
-    diameter = 0.00425
+    diameter = 0.007986
 
     # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
-    height = 0.10634
+    height = 0.050
 
     # Not Used?
     mass = 0.019
 
-    # add by js
-    base_height = 0.0
 
 @configclass
-class Socket_00117(FixedAssetCfg):
-    usd_path = f"{AUTOMATE_ASSET_DIR}/00117_socket/00117_socket.usd"
+class Socket_00000(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00004_socket/00004_socket.usd"
 
     # Not Used?
-    diameter = 0.03863
+    diameter = 0.0081
 
     # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
-    height = 0.06866
+    height = 0.025
     base_height = 0.0
+
+############################################################################################################
 
 @configclass
 class Plug_00004(HeldAssetCfg):
@@ -135,9 +130,6 @@ class Plug_00004(HeldAssetCfg):
     # Not Used?
     mass = 0.019
 
-    # add by js
-    base_height = 0.0
-
 @configclass
 class Socket_00004(FixedAssetCfg):
     usd_path = f"{AUTOMATE_ASSET_DIR}/00004_socket/00004_socket.usd"
@@ -150,60 +142,470 @@ class Socket_00004(FixedAssetCfg):
     base_height = 0.0
 
 @configclass
-class Plug_01132(HeldAssetCfg):
-    usd_path = f"{AUTOMATE_ASSET_DIR}/01132_plug/01132_plug.usd"
+class Plug_00007(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00007_plug/00007_plug.usd"
     
     # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
-    diameter = 0.00887
+    diameter = 0.02148
 
     # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
-    height = 0.09622 # measured value
-    # height = 0.07622 # tmp
+    height = 0.01980
 
     # Not Used?
-    mass = 0.019
+    mass = tmp_mass
 
-    # # add by js
-    base_height = 0.0
-
-@configclass 
-class Socket_01132(FixedAssetCfg):
-    usd_path = f"{AUTOMATE_ASSET_DIR}/01132_socket/01132_socket.usd"
+@configclass
+class Socket_00007(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00007_socket/00007_socket.usd"
 
     # Not Used?
-    diameter = 0.00988
+    diameter = 0.01836
 
     # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
-    height = 0.05660
+    height = 0.04032
     base_height = 0.0
 
 @configclass
-class Plug(HeldAssetCfg):
-    usd_path = f"{AUTOMATE_ASSET_DIR}/00004_plug/00004_plug.usd"
-    diameter = 0.007986
-    height = 0.050
+class Plug_00014(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00014_plug/00014_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00437
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.04217
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00014(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00014_socket/00014_socket.usd"
+
+    # Not Used?
+    diameter = 0.00472
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.00948
+    base_height = 0.0
+
+@configclass
+class Plug_00015(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00015_plug/00015_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00614
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.04191 - 0.01
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00015(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00015_socket/00015_socket.usd"
+
+    # Not Used?
+    diameter = 0.00726
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.01419
+    base_height = 0.0
+
+@configclass
+class Plug_00016(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00016_plug/00016_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00793
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.05024 - 0.005
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00016(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00016_socket/00016_socket.usd"
+
+    # Not Used?
+    diameter = 0.00905
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.01984
+    base_height = 0.0
+
+@configclass
+class Plug_00021(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00021_plug/00021_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00783
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.04333 - 0.01
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00021(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00021_socket/00021_socket.usd"
+
+    # Not Used?
+    diameter = 0.00918
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.00791
+    base_height = 0.0
+
+@configclass
+class Plug_00028(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00028_plug/00028_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00921
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.05895 - 0.01
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00028(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00028_socket/00028_socket.usd"
+
+    # Not Used?
+    diameter = 0.00993
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.02187
+    base_height = 0.0
+
+@configclass
+class Plug_00030(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00030_plug/00030_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00649
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.05283 - 0.01
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00030(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00030_socket/00030_socket.usd"
+
+    # Not Used?
+    diameter = 0.00730
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.00780
+    base_height = 0.0
+
+@configclass
+class Plug_00032(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00032_plug/00032_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00496
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.02000
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00032(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00032_socket/00032_socket.usd"
+
+    # Not Used?
+    diameter = 0.00260
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.02528
+    base_height = 0.0
+
+@configclass
+class Plug_00042(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00042_plug/00042_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00544
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.01926 - 0.005
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00042(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00042_socket/00042_socket.usd"
+
+    # Not Used?
+    diameter = 0.00649
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.00538
+    base_height = 0.0
+
+@configclass
+class Plug_00062(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00062_plug/00062_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.01247
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.05642
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00062(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00062_socket/00062_socket.usd"
+
+    # Not Used?
+    diameter = 0.00875
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.04798
+    base_height = 0.0
+
+@configclass
+class Plug_00074(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00074_plug/00074_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00465
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.04207 + 0.055
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00074(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00074_socket/00074_socket.usd"
+
+    # Not Used?
+    diameter = 0.00580
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.08010
+    base_height = 0.0
+
+@configclass
+class Plug_00077(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00077_plug/00077_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.01995
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    # height = 0.01025 # by measure
+    height = 0.03389 - 0.01025 + 0.015 # by js
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00077(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00077_socket/00077_socket.usd"
+
+    # Not Used?
+    diameter = 0.01022
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.03389
+    base_height = 0.0
+
+@configclass
+class Plug_00078(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00078_plug/00078_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00732
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.05851 - 0.005
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00078(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00078_socket/00078_socket.usd"
+
+    # Not Used?
+    diameter = 0.00878
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.04119
+    base_height = 0.0
+
+@configclass
+class Plug_00081(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00081_plug/00081_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.03496
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    # height = 0.01825 # by measure
+    height = 0.03701 - 0.01825 + 0.005 # by js
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00081(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00081_socket/00081_socket.usd"
+
+    # Not Used?
+    diameter = 0.02364
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.03701
+    base_height = 0.0
+
+@configclass
+class Plug_00083(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00083_plug/00083_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00847
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.03429 - 0.001
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00083(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00083_socket/00083_socket.usd"
+
+    # Not Used?
+    diameter = 0.00379
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.01633
+    base_height = 0.0
+
+@configclass
+class Plug_00103(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00103_plug/00103_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00483
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.08260 - 0.01
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00103(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00103_socket/00103_socket.usd"
+
+    # Not Used?
+    diameter = 0.00623
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.00660
+    base_height = 0.0
+
+@configclass
+class Plug_00110(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00110_plug/00110_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00749
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.07563 - 0.01
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00110(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00110_socket/00110_socket.usd"
+
+    # Not Used?
+    diameter = 0.00551
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.02960
+    base_height = 0.0
+
+@configclass
+class Plug_00117(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00117_plug/00117_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00425
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.10634 - 0.005
+
+    # Not Used?
     mass = 0.019
 
-
 @configclass
-class Socket(FixedAssetCfg):
-    usd_path = f"{AUTOMATE_ASSET_DIR}/00004_socket/00004_socket.usd"
-    diameter = 0.0081
-    height = 0.025
+class Socket_00117(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00117_socket/00117_socket.usd"
+
+    # Not Used?
+    diameter = 0.03863
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.06866
     base_height = 0.0
 
+@configclass
+class Plug_00133(HeldAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00133_plug/00133_plug.usd"
+    
+    # Used for automate_env.py's def _set_franka_to_default_pose() | unit: meter
+    diameter = 0.00882
+
+    # Used for automate_env.py's def get_handheld_asset_relative_pose() | unit: meter
+    height = 0.04334 - 0.005
+
+    # Not Used?
+    mass = tmp_mass
+
+@configclass
+class Socket_00133(FixedAssetCfg):
+    usd_path = f"{AUTOMATE_ASSET_DIR}/00133_socket/00133_socket.usd"
+
+    # Not Used?
+    diameter = 0.00720
+
+    # Used for automate_env.py's def _init_tensors() & def randomize_initial_state() | unit: meter
+    height = 0.02429
+    base_height = 0.0
+
+############################################################################################################
 
 @configclass
 class PlugInsert(AutomateTaskDisassembly):
     name = "plug_insert"
-    # fixed_asset_cfg = Socket()
-    # held_asset_cfg = Plug()
-    # fixed_asset_cfg = Socket_00004()
-    # held_asset_cfg = Plug_00004()
-    fixed_asset_cfg = Socket_00117()
-    held_asset_cfg = Plug_00117()
-    # fixed_asset_cfg = Socket_01132()
-    # held_asset_cfg = Plug_01132()
+    fixed_asset_cfg = Socket_00133()
+    held_asset_cfg = Plug_00133()
     asset_size = 8.0
     duration_s = 10.0
 
