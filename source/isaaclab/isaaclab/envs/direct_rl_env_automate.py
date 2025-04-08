cca446c5b7f1450aded8b09800adac887664bd50
Seunghwan YU <60696265+Hanyang-Robot@users.noreply.github.com>
2025-04-08 14:47:38 +0900

    Integration of JS and SY's code (2025-04-08)
    Signed-off-by: Seunghwan YU <60696265+Hanyang-Robot@users.noreply.github.com>

--- source/isaaclab/isaaclab/envs/direct_rl_env_automate.py
+++ source/isaaclab/isaaclab/envs/direct_rl_env_automate.py
@@ -30,7 +30,8 @@ from isaaclab.utils.noise import NoiseModel
 from isaaclab.utils.timer import Timer
 
 from .common import VecEnvObs, VecEnvStepReturn
-from .direct_rl_env_cfg import DirectRLEnvCfg
+# from .direct_rl_env_cfg import DirectRLEnvCfg
+from .direct_rl_env_automate_cfg import DirectRLEnvAutomateCfg
 from .ui import ViewportCameraController
 from .utils.spaces import sample_space, spec_to_gym_space
 
@@ -328,8 +329,21 @@ class DirectRLEnvAutomate(gym.Env):
         # perform physics stepping
         for _ in range(self.cfg.decimation):
             self._sim_step_counter += 1
-            # set actions into buffers
-            self._apply_action()
+
+            # === [1]. Logging the disassembly path for AutoMate (Edit by CAI-LAB) ==================
+            # set actions into buffers (threshold: 20)
+            if self.episode_length_buf[0] < 20:
+                # [1-1]. Picking up the Plug for Disassembly Path
+                self._apply_action()
+
+            else:
+                # [1-2]. Moving to a Random Position for Disassembly Path
+                self._apply_randomize_action()
+
+            # [1-3]. Logging the disaasembly path
+            self._log_state_per()
+            # =======================================================================================
+
             # set actions into simulator
             self.scene.write_data_to_sim()
             # simulate
@@ -342,10 +356,11 @@ class DirectRLEnvAutomate(gym.Env):
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
 
-        # post-step:
+        # === [2]. Saving the disassembly path for AutoMate (Edit by CAI-LAB) =================
         is_last_step = self._is_last_step()
-        self._post_physics_step(is_last_step)
-        
+        self._log_state(is_last_step)
+        # =====================================================================================
+
         # -- update env counters (used for curriculum generation)
         self.episode_length_buf += 1  # step in current episode (per env)
         self.common_step_counter += 1  # total step (common for all envs)
@@ -619,6 +634,16 @@ class DirectRLEnvAutomate(gym.Env):
         """
         raise NotImplementedError(f"Please implement the '_apply_action' method for {self.__class__.__name__}.")
 
+    # new function for a random disassembly path
+    @abstractmethod
+    def _apply_randomize_action(self):
+        """Apply randomize actions to the simulator.
+
+        This function is responsible for applying the actions to the simulator. It is called at each
+        physics time-step.
+        """
+        raise NotImplementedError(f"Please implement the '_apply_randomize_action' method for {self.__class__.__name__}.")
+
     @abstractmethod
     def _get_observations(self) -> VecEnvObs:
         """Compute and return the observations for the environment.
@@ -668,17 +693,7 @@ class DirectRLEnvAutomate(gym.Env):
         """
         raise NotImplementedError(f"Debug visualization is not implemented for {self.__class__.__name__}.")
 
-    # --- New function for Automate ---------------------------------------------------------------------------------- 
-    @abstractmethod
-    def _post_physics_step(self):
-        """Post-process actions after stepping through the physics.
-
-        This function is responsible for post-processing the actions after stepping through the physics.
-        It is called after the physics stepping (which is decimated).
-
-        """
-        raise NotImplementedError(f"Please implement the '_post_physics_step' method for {self.__class__.__name__}.")
-
+    # === Function for Automate (Edit by CAI-Lab) ====================================================================
     @abstractmethod
     def _is_last_step(self) -> tuple[torch.Tensor]:
         """Compute and return the last_step flags for the environment.
@@ -688,4 +703,18 @@ class DirectRLEnvAutomate(gym.Env):
             Shape of individual tensors is (num_envs,).
         """
         raise NotImplementedError(f"Please implement the '_get_dones' method for {self.__class__.__name__}.")
-    # ----------------------------------------------------------------------------------------------------------------
+
+    @abstractmethod
+    def _log_state(self) -> tuple[torch.Tensor]:
+        """Logging robot_state & object state.
+
+        """
+        raise NotImplementedError(f"Please implement the '_log_state' method for {self.__class__.__name__}.")
+
+    @abstractmethod
+    def _log_state_per(self) -> tuple[torch.Tensor]:
+        """Logging robot_state & object state.
+
+        """
+        raise NotImplementedError(f"Please implement the '_log_state_per' method for {self.__class__.__name__}.")
+    # ================================================================================================================
